2a4a9a916d3006109f8cd5cbfe297fa9715bc9b2
jsuresh <joshua.suresh@gmail.com>
2021-02-10 13:40:27 -0800

    Commit for Jonathan to help troubleshoot

--- setup_sim.py
+++ setup_sim.py
@@ -1,5 +1,6 @@
 from emodpy_malaria.config import set_team_defaults, set_species, get_species_params
 from emod_api.config import default_from_schema_no_validation
+import emodpy_malaria.config as emodpy_malaria_config_module
 
 from jsuresh_helpers.running_dtk import set_executable, add_params_csv_to_dtk_config_builder
 from jsuresh_helpers.uncategorized import load_csv_into_dictionary
@@ -13,6 +14,25 @@ input_folder = dropbox_folder + "projects/jsuresh_sim_examples/example_input_fol
 params_csv_filename = input_folder + "params.csv"
 
 
+
+# def set_log_level(cb, loglevel_default="WARNING"):
+#     # Reduce StdOut size and prevent DTK from spitting out too many messages
+#     cb.update_params({
+#         "logLevel_default": loglevel_default,
+#         "Enable_Log_Throttling": 1,
+#         "Memory_Usage_Warning_Threshold_Working_Set_MB": 50000,
+#         "Memory_Usage_Halting_Threshold_Working_Set_MB": 60000,
+#         "logLevel_JsonConfigurable": "WARNING",
+#         "Disable_IP_Whitelist": 1
+#     })
+#
+#
+# def turn_on_event_recorder(cb, events_to_record):
+#     cb.set_param("Report_Event_Recorder", 1)
+#     cb.set_param("Report_Event_Recorder_Individual_Properties", [])
+
+
+
 def set_ento(config):
     set_species(config, ["arabiensis", "funestus"])
 
@@ -20,14 +40,14 @@ def set_ento(config):
     lhm = default_from_schema_no_validation.schema_to_config_subnode(manifest.schema_file, ["idmTypes","idmType:VectorHabitat"])
     lhm.parameters.Max_Larval_Capacity = 1e10
     lhm.parameters.Vector_Habitat_Type = "TEMPORARY_RAINFALL"
-    # lhm.parameters.finalize()
-    config.get_species_params(config, "arabiensis").Larval_Habitat_Types.append( lhm.parameters )
+    lhm.parameters.finalize()
+    emodpy_malaria_config_module.get_species_params(config, "arabiensis").Larval_Habitat_Types.append( lhm.parameters )
 
     lhm = default_from_schema_no_validation.schema_to_config_subnode(manifest.schema_file, ["idmTypes","idmType:VectorHabitat"])
     lhm.parameters.Max_Larval_Capacity = 1e10
     lhm.parameters.Vector_Habitat_Type = "WATER_VEGETATION"
-    # lhm.parameters.finalize()
-    config.get_species_params(config, "funestus").Larval_Habitat_Types.append( lhm.parameters )
+    lhm.parameters.finalize()
+    emodpy_malaria_config_module.get_species_params(config, "funestus").Larval_Habitat_Types.append( lhm.parameters )
 
 
     # set_species_param #fixme Asking Jonathan to make this function for me
@@ -46,19 +66,25 @@ def set_ento(config):
     '''
 
 
+def set_log_level(config, loglevel_default="WARNING"):
+    # non-schema parameters:
+    config.parameters["logLevel_default"] = loglevel_default,
+    config.parameters["logLevel_JsonConfigurable"] = "WARNING"
+    config.parameters["Enable_Log_Throttling"] = 1
+
+    #schema parameters:
+    config.parameters.Memory_Usage_Warning_Threshold_Working_Set_MB = 50000
+    config.parameters.Memory_Usage_Halting_Threshold_Working_Set_MB = 60000
+
+    return config
+
 def set_project_specific_params(config):
     # config.parameters.Num_Cores = 2  #ERROR: this needs to be done in platform creation
     config.parameters.Climate_Model = "CLIMATE_BY_DATA"
     # config.parameters.Enable_Vector_Migration = 1
 
     set_ento(config)
-
-# def set_larval_habitat() #fixme Unclear if necessary.  Jonathan had this
-#     lhm = dfs.schema_to_config_subnode( manifest.schema_file, ["idmTypes","idmType:VectorHabitat"] )
-#     lhm.parameters.Max_Larval_Capacity = 11250000000
-#     lhm.parameters.Vector_Habitat_Type = "TEMPORARY_RAINFALL"
-#     lhm.parameters.finalize()
-#     conf.get_species_params( config, "gambiae" ).Larval_Habitat_Types.append( lhm.parameters )
+    # set_log_level(config)
 
 
 def build_project_config(config):
@@ -66,6 +92,9 @@ def build_project_config(config):
     config.parameters.Simulation_Type = "MALARIA_SIM"
     # malaria sim defaults
     set_team_defaults(config, manifest)
+
+    # IP white list (non-schema)
+    # config.parameters["Disable_IP_Whitelist"] = 1
 
     # project params
     set_project_specific_params(config)
